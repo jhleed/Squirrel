bbdbf53191735e259d7ad43041790a79aeb9417f
jongholee <jhleed3@gmail.com>
2019-04-07 11:24:14 +0900

    Set Config : Application location

--- src/main/java/com/devtycoon/webservice/SquirrelApplication.java
+++ src/main/java/com/devtycoon/webservice/SquirrelApplication.java
@@ -1,14 +1,20 @@
 package com.devtycoon.webservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class SquirrelApplication {
 
+	private static final String APPLCATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/Users/jongholee/dev/workspace/config/app/Squirrel/real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(SquirrelApplication.class, args);
+		new SpringApplicationBuilder(SquirrelApplication.class)
+				.properties(APPLCATION_LOCATIONS)
+				.run(args);
 	}
 }
